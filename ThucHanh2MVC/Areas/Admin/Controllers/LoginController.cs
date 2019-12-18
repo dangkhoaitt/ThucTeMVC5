133eb91bec74e137c2de2dfe4c3a516407d39bb3
Khoa Võ <dangkhoaitt@gmail.com>
2019-12-18 20:10:23 +0700

    thêm xóa sửa

---
+++ ThucHanh2MVC/Areas/Admin/Controllers/LoginController.cs
@@ -0,0 +1,58 @@
+﻿using Model.Dao;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ThucHanh2MVC.Areas.Admin.Models;
+using ThucHanh2MVC.Common;
+
+namespace ThucHanh2MVC.Areas.Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Admin/Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var dao = new UserDao();
+                var result = dao.Login(model.UserName, Encryptor.MD5Hash(model.PassWord));
+
+                if (result == 1)
+                {
+                    var user = dao.GetById(model.UserName);
+                    var userSession = new UserLogin();
+                    userSession.UserName = user.UserName;
+                    userSession.UserID = user.ID;
+                    Session.Add(CommonStatic.USER_SESSION, userSession);
+                    return RedirectToAction("Index", "Home");
+                }
+                else if(result == 0)
+                {
+                    ModelState.AddModelError("", "Tài khoản không tồn tại");
+                }
+                else if (result == -1)
+                {
+                    ModelState.AddModelError("", "Tài khoản bị khóa");
+                }
+                else if (result == -2)
+                {
+                    ModelState.AddModelError("", "Sai tài khoản hoặc mật khẩu");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Đăng nhập không đúng");
+                }
+            }
+
+            return View("Index");
+        }
+    }
+}
