5cf7299e310a9dcd2365dfd17d087ec406544e5d
Khoa Võ <dangkhoaitt@gmail.com>
2019-12-19 14:13:38 +0700

    Ckeditor

---
+++ tích hơp ck editor/ThucHanh2MVC/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ThucHanh2MVC.Common;
+
+namespace ThucHanh2MVC.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var session = (UserLogin)Session[CommonStatic.USER_SESSION];
+
+            if (session == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new
+                    RouteValueDictionary(new { controller = "Login", action = "Index", Area = "admin" }));
+            }
+            base.OnActionExecuted(filterContext);
+        }
+
+    }
+}
---
+++ tích hơp ck editor/ThucHanh2MVC/Common/UserLogin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThucHanh2MVC.Common
+{
+    [Serializable]
+    public class UserLogin
+    {
+        public long UserID { get; set; }//vì UserID có kiểu bigint là kiểu long trong c#
+        public string UserName { get; set; }
+    }
+}
