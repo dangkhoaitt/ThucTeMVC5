ae5002a1aa7ad65746bd6996dc9bfa36c584d5d4
Khoa Võ <dangkhoaitt@gmail.com>
2019-12-19 21:30:50 +0700

    DropDownlist

---
+++ DropDownList/Model/Dao/UserDao.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+using PagedList;
+
+namespace Model.Dao
+{
+
+    public class UserDao
+    {
+        //Tạo giá trị db ban đầu là null
+        OnlineShopDbContext db = null;
+        public UserDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+
+
+        public long Insert(User entity)
+        {
+            db.Users.Add(entity);
+            db.SaveChanges();
+            return entity.ID;
+        }
+
+        //Thêm mới danh sách User Trả về danh sách tất cả User
+        public IEnumerable<User> ListAllPaging(string searchString, int page, int pageSize)
+        {
+            IQueryable<User> model = db.Users;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                model = model.Where(x => x.UserName.Contains(searchString) || x.Name.Contains(searchString));
+            }
+            return model.OrderByDescending(x => x.CreatedDate).ToPagedList(page, pageSize);
+        }
+
+        //Thêm phương thức Update
+        public bool Update(User entity)
+        {
+            try
+            {
+                var user = db.Users.Find(entity.ID);
+                user.Name = entity.Name;
+                //nếu giá trị pass truyền vào khác rỗng thì kiểm tra
+                if (!String.IsNullOrEmpty(entity.Password))
+                {
+                    user.Password = entity.Password;
+                }
+                user.Address = entity.Address;
+                user.Email = entity.Email;
+                user.ModifiedBy = entity.ModifiedBy;
+                user.ModifiedDate = DateTime.Now;
+                db.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                //loging
+                return false;
+            }
+        }
+
+        //tạo phương thức lấy ra User
+        public User GetById(string userName)
+        {
+            return db.Users.SingleOrDefault(x => x.UserName == userName);
+        }
+
+        //Tạo phương thức lấy ra id
+        public User ViewDetail(int id)
+        {
+            return db.Users.Find(id);
+        }
+
+        public int Login(string userName, string passWord)
+        {
+            //tạo truy vấn lấy ra một số nguyên tương ứng với số user và password
+            var result = db.Users.SingleOrDefault(x => x.UserName == userName);
+            if (result == null)
+            {
+                return 0;
+            }
+            else
+            {
+                if (result.Status == false)
+                {
+                    return -1;
+                }
+                else
+                {
+                    if (result.Password == passWord)
+                        return 1;
+                    else
+                        return -2;
+                }
+            }
+        }
+
+        //tạo hàm delete
+        public bool Delete(int id)
+        {
+            try
+            {
+                var user = db.Users.Find(id);
+                db.Users.Remove(user);
+                db.SaveChanges();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+            
+        }
+    }
+}
---
+++ DropDownList/ThucHanh2MVC/Common/CommonStatic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ThucHanh2MVC.Common
+{
+    public static class CommonStatic
+    {
+        public static string USER_SESSION = "USER_SESSION";
+    }
+}
