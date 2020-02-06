135add1b460fe0a00ca69e6f5cd0450be6b6bee7
Khoa Võ <dangkhoaitt@gmail.com>
2020-02-06 19:49:10 +0700

    Tối ưu SEO cho website bằng cách tạo tag

---
+++ Tạo trang tìm kiếm/Model/Dao/CategoryDao.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.EF;
+
+namespace Model.Dao
+{
+    public class CategoryDao
+    {
+        OnlineShopDbContext db = null;
+        public CategoryDao()
+        {
+            db = new OnlineShopDbContext();
+        }
+        public List<Category> ListAll()
+        {
+            return db.Categories.Where(x => x.Status == true).ToList();
+        }
+
+        public List<Trademark> ListTrade()
+        {
+            return db.Trademarks.OrderBy(x => x.BrandId).ToList();
+        }
+
+        public ProductCategory ViewDetail(long id)
+        {
+            return db.ProductCategories.Find(id);
+        }
+
+        public long Insert(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+            return category.ID;
+        }
+    }
+}
---
+++ Tạo trang tìm kiếm/ThucHanh2MVC/Areas/Admin/Controllers/BaseController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using ThucHanh2MVC.Common;
+
+
+namespace ThucHanh2MVC.Areas.Admin.Controllers
+{
+    public class BaseController : Controller
+    {
+        //initilizing culture on controller initilization
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            if(Session[CommonStatic.CurrentCulture] != null)
+            {
+                Thread.CurrentThread.CurrentCulture = new CultureInfo(Session[CommonStatic.CurrentCulture].ToString());
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo(Session[CommonStatic.CurrentCulture].ToString());
+            }
+            else
+            {
+                Session[CommonStatic.CurrentCulture] = "vi";
+                Thread.CurrentThread.CurrentCulture = new CultureInfo("vi");
+                Thread.CurrentThread.CurrentUICulture = new CultureInfo("vi");
+            }
+        }
+
+        //changing culture
+        public ActionResult ChangeCulture(string ddlCulture,string returnUrl)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(ddlCulture);
+            Thread.CurrentThread.CurrentUICulture = new CultureInfo(ddlCulture);
+
+            Session[CommonStatic.CurrentCulture] = ddlCulture;
+            return Redirect(returnUrl);
+        }
+
+
+        protected override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            var session = (UserLogin)Session[CommonStatic.USER_SESSION];
+
+            if (session == null)
+            {
+                filterContext.Result = new RedirectToRouteResult(new
+                    RouteValueDictionary(new { controller = "Login", action = "Index", Area = "admin" }));
+            }
+            base.OnActionExecuted(filterContext);
+        }
+
+        protected void SetAlert(string message, string type)
+        {
+            TempData["AlertMessage"] = message;
+            if (type == "success")
+            {
+                TempData["AlertType"] = "alert-success";
+            }
+            else if(type == "warning")
+            {
+                TempData["AlertType"] = "alert-warning";
+            }
+            else if(type == "error")
+            {
+                TempData["AlertType"] = "alert-danger";
+            }
+        }
+
+        
+
+    }
+}
